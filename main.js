95679fc6d2e53bf8c2734f20017dabbeb88b15d5
Mirfozil <mirfoziln@gmail.com>
2026-05-25 09:33:21 +0500

    finish

---
+++ main.js
@@ -0,0 +1,105 @@
+// 1
+const userName = "Mirfozil";
+console.log(userName, typeof userName);
+// 2
+let age = 20;
+console.log(age, typeof age);
+age += 5;
+console.log(age);
+// 3
+const PI = 3.14;
+console.log(PI, typeof PI);
+// 4
+const a = 10;
+const b = 5;
+const sum = a + b;
+console.log(sum);
+// 5
+const str = "Hello";
+const num = 42;
+const bool = true;
+let undef;
+const empty = null;
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof bool);
+console.log(typeof undef);
+console.log(typeof empty);
+// 6
+var nameVar = "Ali";
+nameVar = "Vali";
+console.log(nameVar);
+// 7
+let birthYear = 2005;
+const currentYear = 2025;
+const age1 = currentYear - birthYear;
+console.log(age1);
+// 8
+const ism = "Ali";
+const familiya = "Valiyev";
+const fullName = ism + " " + familiya;
+console.log(fullName);
+// 9
+console.log(`Salom, men ${ism}man, yoshim ${age1}`);
+// 10
+let x;
+console.log(x);
+// 11
+let y = null;
+console.log(y);
+// 12
+const name2 = "Ali";
+const age2 = 22;
+const isStudent = true;
+console.log(typeof name2);
+console.log(typeof age2);
+console.log(typeof isStudent);
+// 13
+const text = "JavaScript";
+console.log(text.length);
+// 14
+const strNum = "25";
+const convertedNum = Number(strNum);
+console.log(convertedNum, typeof convertedNum);
+// 15
+const numberVal = 100;
+const convertedStr = String(numberVal);
+console.log(convertedStr, typeof convertedStr);
+// 16
+const num1 = 7;
+const result = num1 * 3;
+console.log(result);
+// 17
+const hello = "Hello";
+const world = "World!";
+console.log(hello + " " + world);
+// 18
+let rang = "qizil";
+rang = "ko‘k";
+console.log(rang);
+// 19
+const userFirstName = "Ali";
+const userLastName = "Karimov";
+const userAge = 25;
+console.log(`Ism: ${userFirstName}, Familiya: ${userLastName}, Yosh: ${userAge}`);
+// 20
+const isTrue = true;
+const isFalse = false;
+console.log(isTrue, isFalse);
+// 21
+console.log(typeof null);       
+console.log(typeof undefined);  
+// 22
+console.log('I am learning JavaScript');
+// 23
+let son = 5;
+son++;
+console.log(son);
+// 24
+let ism3 = "Ali", yosh3 = 20, kasb = "Developer";
+console.log(`Ism: ${ism3}, Yosh: ${yosh3}, Kasb: ${kasb}`);
+// 25
+let birthYear1 = 2000;
+let current = 2025;
+let ageCalc = current - birthYear1;
+console.log(`Sizning yoshingiz: ${ageCalc}`);
